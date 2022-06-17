0c7aaaf7574b35f151b934a29323d409b382928c
nobodyguy <4577368+nobodyguy@users.noreply.github.com>
2022-06-17 18:45:34 +0000

    Increased backend data refetch interval

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,14 @@ import "./theme/variables.css";
 
 setupIonicReact();
 
-const queryClient: QueryClient = new QueryClient();
+const queryClient: QueryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 5 * 60 * 1000, // 5 minutes
+            cacheTime: 10 * 60 * 1000, // 10 minutes
+        },
+    },
+});
 
 const App: React.FC = () => (
     <QueryClientProvider client={queryClient}>
